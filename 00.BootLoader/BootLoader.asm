ce60aa02ef2c3234f95b0e4ed6b35c9576bd6476
Einstrasse <hg9587@naver.com>
2018-01-03 03:58:55 +0900

    Boot Loader with Screen Wiper Code with assembly.

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -3,9 +3,32 @@
 
 SECTION .text
 
-jmp $
+jmp 0x07C0:START
 
-times 510 - ( $ - $$ )	db	0x00
+START:
+	mov ax, 0x07C0
+	mov ds, ax
+	mov ax, 0xB800
+	mov es, ax
+
+	mov si, 0
+
+.SCREENCLEARLOOP:
+
+	mov byte [ es: si ], 0x00
+	mov byte [ es: si+1 ], 0x0A
+
+	add si, 2
+	cmp si, 80*25*2
+
+	jl .SCREENCLEARLOOP
+
+	mov si, 0
+	mov di, 0
+
+	jmp $
+
+	times 510 - ( $ - $$ )	db	0x00
 
 db 0x55		;
 db 0xAA		;
